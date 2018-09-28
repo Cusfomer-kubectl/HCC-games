b625d17a2f326a59a522d5a6bef4bd9b4302f951
Cusfomer-kubectl <562741542@qq.com>
2018-09-28 06:20:14 +0000

    bugfix

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 
-cd jumpone_8080/
+cd /root/jumpone_8080/
 http-server &
 sleep 1
 
-cd ../pacman_8081/
+cd /root/pacman_8081/
 http-server &
 sleep 1
 
-cd ../wuziqi_8082/
+cd /root/wuziqi_8082/
 http-server &
 sleep 1
 
-cd ../brickball_8083/
+cd /root/brickball_8083/
 http-server &
 sleep 1
 
-cd ../fctank_8084/
+cd /root/fctank_8084/
 http-server &
 sleep 1
