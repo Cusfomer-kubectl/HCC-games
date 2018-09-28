0828ae10b015891ed1bc477fb1f3514cf7d629c5
Cusfomer-kubectl <562741542@qq.com>
2018-09-28 03:57:24 +0000

    start.sh add sleep

--- start.sh
+++ start.sh
@@ -1,15 +1,20 @@
 
 cd jumpone_8080/
 http-server &
+sleep 1
 
 cd ../pacman_8081/
 http-server &
+sleep 1
 
 cd ../wuziqi_8082/
 http-server &
+sleep 1
 
 cd ../brickball_8083/
 http-server &
+sleep 1
 
 cd ../fctank_8084/
 http-server &
+sleep 1
